40877afaafd3f6fcf1b69be3518d35f7f3d0c206
David <friday10292001@yahoo.com.tw>
2020-10-13 17:36:40 +0800

    component of list

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -21,6 +21,7 @@ import 'styles'
 
 //vue
 import Vue from 'vue/dist/vue.esm';
+import List from 'components/list.vue';
 
 document.addEventListener("turbolinks:load", function(event){
   let el = document.querySelector("#board");
@@ -29,7 +30,8 @@ document.addEventListener("turbolinks:load", function(event){
       el,
       data: {
         lists: JSON.parse(el.dataset.lists)
-      }
+      },
+      components: { List }
     });
   }
 })
