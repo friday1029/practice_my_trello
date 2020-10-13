a08fac5802a7bf01bbe858f81f97e546b1979a7e
David <friday10292001@yahoo.com.tw>
2020-10-13 20:19:19 +0800

    component of card

---
+++ app/frontend/components/card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    {{card.name}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'Card', 
+  props: ["card"], 
+}
+</script>
+<style lang="scss" scoped>
+  .card {
+    @apply .bg-gray-100 .mb-2 .px-2 .py-2 .rounded-sm;
+  }
+</style>
